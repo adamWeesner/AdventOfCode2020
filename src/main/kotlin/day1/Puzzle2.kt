8b6530426c0d8bf98ecebde1a9aaaee080a9848a
Adam Weesner <alweesner@gmail.com>
2020-12-07 18:27:57 -0600

    added day 1 puzzle 2

---
+++ src/main/kotlin/day1/Puzzle2.kt
@@ -0,0 +1,22 @@
+package day1
+
+import read
+
+fun main() {
+    val expenses = "day1/puzzle1Data.txt".read()
+    val items = sumIs2020(expenses)
+    println("summed to 2020: ${items.first}, ${items.second}, ${items.third}")
+    println("multiplication: ${items.first * items.second * items.third}")
+}
+
+private fun sumIs2020(expenses: List<String>): Triple<Int, Int, Int> {
+    expenses.forEachIndexed { index, expense ->
+        expenses.subList(index + 1, expenses.lastIndex).forEachIndexed { index2, expense2 ->
+            expenses.subList(index2 + 1, expenses.lastIndex).forEach { expense3 ->
+                val sum = expense.toInt() + expense2.toInt() + expense3.toInt()
+                if (sum == 2020) return Triple(expense.toInt(), expense2.toInt(), expense3.toInt())
+            }
+        }
+    }
+    return Triple(0, 0, 0)
+}
